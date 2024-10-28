aea7c8abe51b7ad1e2a6344a95be5939f786d056
Vikas Kapadiya <vikas@kapadiya.net>
2024-10-28 19:19:29 +0400

    content fix

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 export const metaData = {
   baseUrl: "https://astrofolio-mu.vercel.app/",
   title: "Astrofolio",
-  name: "Sirius",
+  name: "Vikas",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
+  twitter: "https://x.com/vikas5914",
+  github: "https://github.com/vikas5914/Astrofolio",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
   email: "mailto:example@gmail.com",
---
+++ src/pages/atom.xml.js
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { metaData } from "./../config";
+
+console.log(metaData);
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    title: metaData.title,
+    description: metaData.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
