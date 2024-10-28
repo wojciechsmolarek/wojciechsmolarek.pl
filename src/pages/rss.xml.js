aae0c0572b022126485975f2ba7534686d43f909
Vikas Kapadiya <vikas@kapadiya.net>
2024-10-28 16:27:12 +0400

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { metaData } from "./../config";
+
+console.log(metaData);
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: metaData.title,
+    description: metaData.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
 }
