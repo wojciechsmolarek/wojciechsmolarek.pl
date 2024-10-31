17909814590b0ed4df8b93aca95bc28e9d730755
Vikas Kapadiya <vikas@kapadiya.net>
2024-10-31 13:16:06 +0400

    og:testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,9 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-
 import vercel from "@astrojs/vercel/static";
-
 import icon from "astro-icon";
+import opengraphImages, { presets } from "astro-opengraph-images";
 
 // https://astro.build/config
 export default defineConfig({
@@ -53,14 +52,29 @@ export default defineConfig({
       },
     }),
     sitemap(),
+    opengraphImages({
+      render: presets.waveSvg,
+      options: {
+        fonts: [
+          {
+            name: "Roboto",
+            name: "Roboto",
+            name: "Roboto",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync(
+              "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"
+            ),
+          },
+        ],
+      },
+    }),
   ],
-
   output: "static",
   adapter: vercel({
-    isr: true,
     webAnalytics: {
       enabled: true,
     },
-    imageService: true,
+    imageService: false,
   }),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,24 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ['class', '[data-theme="dark"]'],
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		  fontFamily: {
-			sans: ['Geist Sans', ...defaultTheme.fontFamily.sans],
-			mono: ['Geist Mono', ...defaultTheme.fontFamily.mono],
-		  },
-		  typography: {
-			quoteless: {
-			  css: {
-				"blockquote p:first-of-type::before": { content: "none" },
-				"blockquote p:first-of-type::after": { content: "none" },
-			  },
-			},
-		  },
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+        mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
+      },
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
