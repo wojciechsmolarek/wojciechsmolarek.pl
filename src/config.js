5efe125ee2792d5f50814e0695951b06eefcc6ac
Wojciech <wojciech@MacBookAir.lan>
2024-11-10 19:17:20 +0100

    Update homepage and footer, basic config

--- src/config.js
+++ src/config.js
@@ -1,16 +1,15 @@
 export const metaData = {
-  baseUrl: "https://astrofolio-astro.vercel.app/",
-  title: "Astrofolio",
-  name: "Vikas",
+  baseUrl: "https://wojciechsmolarek.pl/",
+  title: "WojciechSmolarek.pl",
+  name: "Wojciech",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/vikas5914",
-  github: "https://github.com/vikas5914/Astrofolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/wojciechsmolarek",
+  instagram: "https://www.instagram.com/wojciehc/",
+  linkedin: "https://www.linkedin.com/in/wojciech-smolarek/",
+  email: "mailto:wojteksmolarek7@gmail.com",
 };
