1d2d7cccd133f899cb1a90b0ce80073dd31b2c18
Wojciech Smolarek <wojteksmolarek7@gmail.com>
2024-12-20 11:32:53 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -83,7 +83,7 @@ export default defineConfig({
   output: "static",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: false,
     },
     imageService: true,
     devImageService: "sharp",
