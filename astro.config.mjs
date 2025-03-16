b0aa845b016cf7c5ce093716d729d5af0bf42307
Wojciech Smolarek <kontakt@wojiechsmolarek.pl>
2025-03-16 16:48:09 +0100

    Build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,7 @@ import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
-import opengraphImages, { presets } from "astro-opengraph-images";
-import fs from 'fs';
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.wojciechsmolarek.pl/",
   trailingSlash: 'never',
@@ -60,21 +57,6 @@ export default defineConfig({
         "fa6-solid": ["rss", "circle-half-stroke", "caret-down", "bars"],
         tabler: ["mail-filled"],
         "fa6-brands": ["x-twitter", "github", "instagram", "linkedin-in"],
-      },
-    }),
-    opengraphImages({
-      render: presets.waveSvg,
-      options: {
-        fonts: [
-          {
-            name: "Roboto",
-            weight: 400,
-            style: "normal",
-            data: fs.readFileSync(
-              "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"
-            ),
-          },
-        ],
       },
     }),
   ],
